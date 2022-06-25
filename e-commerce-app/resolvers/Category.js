456964728a1c77290b590098dbf3b5654db2fccb
sritaj <sritajp@gmail.com>
2022-06-25 13:43:25 +0530

    Updated Category Query for filtering based on avgRating and onSale params

--- e-commerce-app/resolvers/Category.js
+++ e-commerce-app/resolvers/Category.js
@@ -1,6 +1,38 @@
 // resolver for Category
 exports.Category = {
-  products: ({ id: categoryID }, args, { products }) => {
-    return products.filter((product) => product.categoryID === categoryID);
+  products: ({ id: categoryID }, { filter }, { products, reviews }) => {
+    const categoryProducts = products.filter(
+      (product) => product.categoryID === categoryID
+    );
+    let filteredCategoryProducts = categoryProducts;
+
+    if (filter) {
+      const { onSale, avgRating } = filter;
+      if (onSale === true) {
+        filteredCategoryProducts = filteredCategoryProducts.filter(
+          (product) => {
+            return product.onSale;
+          }
+        );
+      }
+      if ([1, 2, 3, 4, 5].includes(avgRating)) {
+        filteredCategoryProducts = filteredCategoryProducts.filter(
+          (product) => {
+            let sumRating = 0;
+            let numberOfReviews = 0;
+            reviews.forEach((review) => {
+              if (review.productId === product.id) {
+                sumRating += review.rating;
+                numberOfReviews++;
+              }
+            });
+            console.log(sumRating, product.name);
+            const avgProductRating = sumRating / numberOfReviews;
+            return avgProductRating >= avgRating;
+          }
+        );
+      }
+      return filteredCategoryProducts;
+    }
   },
 };
