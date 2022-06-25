c6185895df80c7c0b41c9bf1c4f095edee8aa983
sritaj <sritajp@gmail.com>
2022-06-25 13:43:11 +0530

    Updated Product Query for filtering based on avgRating and onSale params

--- e-commerce-app/resolvers/Query.js
+++ e-commerce-app/resolvers/Query.js
@@ -1,6 +1,34 @@
+const { reviews } = require("../db");
+
 exports.Query = {
-  products: (parent, args, { products }) => {
-    return products;
+  products: (parent, { filter }, { products }) => {
+    let filteredProducts = products;
+
+    if (filter) {
+      const { onSale, avgRating } = filter;
+      if (onSale === true) {
+        filteredProducts = filteredProducts.filter((product) => {
+          return product.onSale;
+        });
+      }
+      if ([1, 2, 3, 4, 5].includes(avgRating)) {
+        filteredProducts = filteredProducts.filter((product) => {
+          let sumRating = 0;
+          let numberOfReviews = 0;
+          reviews.forEach((review) => {
+            if (review.productId === product.id) {
+              sumRating += review.rating;
+              numberOfReviews++;
+            }
+          });
+          console.log(sumRating, product.name);
+          const avgProductRating = sumRating / numberOfReviews;
+          return avgProductRating >= avgRating;
+        });
+      }
+    }
+
+    return filteredProducts;
   },
   // resolver for product based on id
   product: (parent, { id: productID }, { products }) => {
