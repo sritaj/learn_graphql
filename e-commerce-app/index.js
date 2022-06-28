c4b3ce57e9a6ceb6c9ceb3b12e9de49c684fe52d
sritaj <sritajp@gmail.com>
2022-06-28 13:00:21 +0530

    Added Mutation in index.js

--- e-commerce-app/index.js
+++ e-commerce-app/index.js
@@ -2,6 +2,7 @@ const { ApolloServer, gql } = require("apollo-server");
 const { typeDefs } = require("./schema");
 const { products, categories, reviews } = require("./db");
 const { Query } = require("./resolvers/Query");
+const { Mutation } = require("./resolvers/Mutation");
 const { Category } = require("./resolvers/Category");
 const { Product } = require("./resolvers/Product");
 
@@ -9,6 +10,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers: {
     Query,
+    Mutation,
     Category,
     Product,
   },
