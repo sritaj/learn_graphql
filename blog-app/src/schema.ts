ce693bb4de380305d7d13108c9c3a00de7d25f42
sritaj <sritajp@gmail.com>
2022-07-09 19:48:50 +0530

    Updated Mutation params/args

--- blog-app/src/schema.ts
+++ blog-app/src/schema.ts
@@ -7,7 +7,7 @@ export const typeDefs = gql`
 
   type Mutation {
     postCreate(post: PostInput!): PostPayload!
-    postUpdate(post: PostInput): PostPayload!
+    postUpdate(postId: ID!, post: PostInput): PostPayload!
   }
 
   type Post {
