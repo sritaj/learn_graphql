6a081a481d9c6616581878bce21d0ad0efb8f58a
sritaj <sritajp@gmail.com>
2022-07-11 00:20:47 +0530

    Added query to fetch posts and updated the return statement with attributes

--- blog-app-frontend/src/pages/Posts/Posts.js
+++ blog-app-frontend/src/pages/Posts/Posts.js
@@ -1,6 +1,49 @@
 import React from "react";
 import Post from "../../components/Post/Post";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_POST = gql`
+  query {
+    posts {
+      id
+      title
+      content
+      createdAt
+      user {
+        id
+        name
+        email
+      }
+    }
+  }
+`;
 
 export default function Posts() {
-  return <div></div>;
+  const { data, error, loading } = useQuery(GET_POST);
+
+  console.log({ data, error, loading });
+
+  if (error) return <div>Error Page</div>;
+
+  if (loading) return <div>Loading ...</div>;
+
+  const { posts } = data;
+
+  return (
+    <div>
+      {posts.map((post) => {
+        return (
+          <Post
+            title={post.title}
+            content={post.content}
+            createdAt={post.createdAt}
+            id={post.id}
+            user={post.user.name}
+            email={post.user.email}
+            userId={post.user.id}
+          />
+        );
+      })}
+    </div>
+  );
 }
