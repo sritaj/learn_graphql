358605e75ff74f55f6b0af133d0803e980e0d982
sritaj <sritajp@gmail.com>
2022-07-10 17:15:04 +0530

    Added Query for retrieving current user/me info

--- blog-app/src/resolvers/Query.ts
+++ blog-app/src/resolvers/Query.ts
@@ -1,6 +1,14 @@
 import { Context } from "..";
 
 export const Query = {
+  me: (_: any, __: any, { prisma, userInfo }: Context) => {
+    if (!userInfo) return null;
+    return prisma.user.findUnique({
+      where: {
+        id: userInfo.userID,
+      },
+    });
+  },
   posts: async (_: any, __: any, { prisma }: Context) => {
     const posts = await prisma.post.findMany({
       orderBy: [
