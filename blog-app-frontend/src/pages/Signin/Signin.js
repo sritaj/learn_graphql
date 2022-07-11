7d01afe0c01f52e14e4d13bf5e3a8d3073b4de78
sritaj <sritajp@gmail.com>
2022-07-12 00:44:50 +0530

    Added capability to signin and retrieve token and set it in application(console tab)

--- blog-app-frontend/src/pages/Signin/Signin.js
+++ blog-app-frontend/src/pages/Signin/Signin.js
@@ -2,14 +2,46 @@ import React, { useState, useEffect } from "react";
 
 import { Form } from "react-bootstrap";
 import Button from "@restart/ui/esm/Button";
+import { gql, useMutation } from "@apollo/client";
+
+const SIGNIN = gql`
+  mutation ($email: String!, $password: String!) {
+    signin(credentials: { password: $password, email: $email }) {
+      userErrors {
+        message
+      }
+      token
+    }
+  }
+`;
 
 export default function Signin() {
+  const [signin, { data, loading }] = useMutation(SIGNIN);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    signin({
+      variables: {
+        email,
+        password,
+      },
+    });
+  };
 
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (data) {
+      if (data.signin.userErrors.length) {
+        setError(data.signin.userErrors[0].message);
+      }
+      if (data.signin.token) {
+        localStorage.setItem("token", data.signin.token);
+      }
+    }
+  }, [data]);
 
   return (
     <div>
