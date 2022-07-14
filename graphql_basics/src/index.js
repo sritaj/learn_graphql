02b6a2da1b38c64596659f520589dcb793c0cae2
sritaj <sritajp@gmail.com>
2022-07-14 23:21:07 +0530

    Added Query example with Array input

--- graphql_basics/src/index.js
+++ graphql_basics/src/index.js
@@ -1,1 +1,30 @@
-console.log("Welcome to GraphQL");
+import { createServer } from "@graphql-yoga/node";
+
+const typeDefinitions = /* GraphQL */ `
+  type Query {
+    sum(numbers: [Float!]!): Float!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sum: (parent, args, context, info) => {
+      if (args.numbers.length == 0) return 0;
+
+      return args.numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      });
+    },
+  },
+};
+
+const server = createServer({
+  schema: {
+    typeDefs: [typeDefinitions],
+    resolvers: [resolvers],
+  },
+});
+
+server.start(() => {
+  console.log("Server is up");
+});
