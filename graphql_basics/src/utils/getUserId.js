d4f02533a92936dcd7e3ffa10ef3b5aab6153429
sritaj <sritajp@gmail.com>
2022-07-25 00:33:42 +0530

    Updated getUserId method to check for authentication if the default value is present

--- graphql_basics/src/utils/getUserId.js
+++ graphql_basics/src/utils/getUserId.js
@@ -3,18 +3,22 @@ import { GraphQLYogaError } from "@graphql-yoga/node";
 
 const JWT_AUTH_TOKEN = "MyAccessTokenForBlogProject";
 
-const getUserId = (request) => {
-  const header = request.extensions.headers.Authorization;
+const getUserId = (request, requireAuth = true) => {
+  if (requireAuth) {
+    const header = request.extensions.headers.Authentication;
 
-  if (!header) {
-    throw new GraphQLYogaError("Authentication Required!!");
+    if (header) {
+      const token = header.replace("Bearer ", "");
+      const decoded = JWT.verify(token, JWT_AUTH_TOKEN);
+      return decoded.id;
+    }
+
+    if (!header) {
+      throw new GraphQLYogaError("Authentication Required!!");
+    }
   }
 
-  const token = header.replace("Bearer ", "");
-
-  const decoded = JWT.verify(token, JWT_AUTH_TOKEN);
-
-  return decoded.id;
+  return null;
 };
 
 export { getUserId as default };
